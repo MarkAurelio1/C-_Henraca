0856957ac5fdd9a64482e52d9d992b76c24a5e74
Marcos Aurelio <126610050+MarkAurelio1@users.noreply.github.com>
2023-04-24 20:14:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Animal
+    {
+        //Propriedades e métodos da classe Animal
+    }
+
+    class Cachorro : Animal
+    {
+        //Propriedades e métodos da classe Cachorro
+    }
+    //Herança é um conceito fundamental da programação orientada
+    //a objetos que permite que uma classe herde propriedades e
+    //comportamentos de outra classe.
+
+    //Neste exemplo, a classe "Cachorro"
+    //herda todas as propriedades e métodos da classe
+    //"Animal". Isso significa que podemos usar todos os métodos e propriedades da classe "Animal" na classe "Cachorro", além de adicionar métodos e propriedades adicionais específicos da classe "Cachorro"
+
+
+}
